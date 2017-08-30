7d5d5f5594c8f3b517be1e9e7d4b8861a19e15a2
nastasza <30432970+nastasza@users.noreply.github.com>
2017-08-31 00:59:25 +0800

    Add files via upload

---
+++ Chap 1/values.js
@@ -0,0 +1,91 @@
+/* 1) number*/
+
+13
+/*13*/
+9.81
+/*9.81*/
+2.999e8
+/*299900000*/
+100 + 4 * 11
+/*144*/
+(100 + 4) * 11
+/*1144*/
+1 - 2 + 1
+/*0*/
+314 % 100
+/*14*/
+144 % 12
+/*0*/
+0 / 0
+/*NaN (Not a Number)*/
+
+/* 2) strings*/
+
+"Patch my boat with chewing gum"
+/*"Patch my boat with chewing gum"*/
+'Monkeys wave goodbye'
+/*"Monkeys wave goodbye"*/
+"This is the first line\nAnd this is the second"
+/*"This is the first line*/
+/*And this is the second"*/
+"A newline character is written like \"\\n\"."
+/*"A newline character is written like "\n"."*/
+"con" + "cat" + "e" + "nate"
+/*"concatenate"*/
+console.log(typeof 4.5)
+/*number*/
+console.log(typeof "x")
+/*string*/
+console.log(typeof "x")
+/*string*/
+
+/* 3) booleans*/
+
+console.log(3 > 2)
+/*true*/
+console.log(3 < 2)
+/*false*/
+console.log("Aardvark" < "Zoroaster")
+/*true*/
+console.log("Itchy" != "Scratchy")
+/*true*/
+console.log(NaN == NaN)
+/*false*/
+console.log(true && false)
+/*false*/
+console.log(true && true)
+/*true*/
+console.log(false || true)
+/*true*/
+console.log(false || false)
+/*false*/
+console.log(true ? 1 : 2);
+/*1*/
+console.log(false ? 1 : 2);
+/*2*/
+
+
+/* 4) objets*/
+
+/* 5) functions*/
+
+/* 6) undefined values*/
+
+console.log(8 * null)
+/*0*/
+console.log("5" - 1)
+/*4*/
+console.log("5" + 1)
+/*51*/
+console.log("five" * 2)
+/*NaN*/
+console.log(false == 0)
+/*true*/
+console.log(null == undefined);
+/*true*/
+console.log(null == 0);
+/*false*/
+console.log(null || "user")
+/*user*/
+console.log("Karl" || "user")
+/*Karl*/
